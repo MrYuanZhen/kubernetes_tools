35fc55fdc9868056c414e95bcae80e3ea3a09ffd
zhen <535875999@qq.com>
2021-04-27 20:07:32 +0800

    update Deployment

--- endpoint.sh
+++ endpoint.sh
@@ -4,8 +4,6 @@ haproxy -f /usr/local/etc/haproxy/haproxy.cfg -p /run/haproxy.pid -D -sf
 
 cat /etc/kubernetes/ssl/kube-node.pem /etc/kubernetes/ssl/kube-node-key.pem > /home/ssl-crt.pem
 
-echo $CONFDAGE && echo $RISETIME
-
 clusterGetnodeip -rise=$RISETIME -confd-arg="${CONFDAGE}"
 
 #export ENDPOINTS=$( kubectl get node -l node-role.kubernetes.io/controlplane=true --no-headers -owide | awk '{print $6}' | awk '{{printf"%s,",$0}}' | sed s'/.$//' )
