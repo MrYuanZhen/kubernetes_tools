0e4732bb45c74d145e53d66cab4f8aa9ad83fe2f
zhen <535875999@qq.com>
2021-04-27 21:47:04 +0800

    update confd arg

--- clusterGetnodeip/OperateSystem.go
+++ clusterGetnodeip/OperateSystem.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
 	"os/exec"
 )
@@ -16,18 +15,18 @@ func setEnv(ipStr string) {
 func operateConfd(ipStr string, confdArg string) {
 
 	//get controller NoeIP env
-	var MASTERIPS string
-	MASTERIPS = os.Getenv("ENDPOINTS")
-	fmt.Println("当前准备执行更新的master节点IP为：", MASTERIPS)
+	//var MASTERIPS string
+	//MASTERIPS = os.Getenv("ENDPOINTS")
+	//fmt.Println("当前准备执行更新的master节点IP为：", MASTERIPS)
 
 	// run confd process
-	cmd := exec.Command("ENDPOINTS="+ipStr, "confd", confdArg)
-	out, err := cmd.CombinedOutput()
+	cmd := "ENDPOINTS=" + ipStr + " confd " + confdArg
+	out, err := exec.Command("bash", "-c", cmd).Output()
+	//cmd := exec.Command("ENDPOINTS="+ipStr, "confd", confdArg)
+	//out, err := cmd.CombinedOutput()
 	if err != nil {
-		log.Fatalf("cmd.Run() failed with %s\n", err)
+		fmt.Printf("Failed to execute command: %s", cmd)
 	}
-	fmt.Printf("combined out:\n%s\n", string(out))
-
-	fmt.Println("执行更新confd命令：", cmd)
+	fmt.Println(string(out))
 
 }
